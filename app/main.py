1da758609820cba9d90cec8151c6c60e81e5f9fb
RKChrist <roo12ney@gmail.com>
2022-03-16 11:22:17 +0100

    Dockerfile

--- app/main.py
+++ app/main.py
@@ -1,9 +1,7 @@
 
-import py_compile
-import uvicorn
+
 from fastapi import FastAPI
 import pandas as pd
-import PyPDF2
 from pathlib import Path
 
 app = FastAPI()
@@ -11,7 +9,9 @@ app = FastAPI()
 @app.get("/files/{file_path}")
 async def return_Pdf(file_path: str):
     base_path = Path(__file__).parent
-    files_path = (base_path / "../FastAPI/Files/Wöldike_1set.pdf").resolve() 
+    substring = base_path.resolve()
+    print(base_path)
+    files_path = (substring / "/FastAPI/Files/Wöldike_1set.pdf").resolve() 
     
     files = {"file": open(files_path, 'rb').read().decode(encoding='utf-8', errors='ignore')}
     
